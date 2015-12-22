01de1a9d506ff51bff4100e11275557226fa8b9a
Jonathan Ma <jma@Jonathans-MacBook-Pro.local>
2015-12-22 01:19:43 -0500

    fix bug

--- youtube_dl/YoutubeDL.py
+++ youtube_dl/YoutubeDL.py
@@ -1683,11 +1683,32 @@ class YoutubeDL(object):
                 raise
             else:
                 if self.params.get('dump_single_json', False):
-                    return_obj['json'] = res
+                    self.to_stdout(json.dumps(res))
+        return self._download_retcode
+    def download_joma(self, url_list):
+        """Download a given list of URLs."""
+        outtmpl = self.params.get('outtmpl', DEFAULT_OUTTMPL)
+        return_obj = {}
+        if (len(url_list) > 1 and
+                '%' not in outtmpl and
+                self.params.get('max_downloads') != 1):
+            raise SameFileError(outtmpl)
+
+        for url in url_list:
+            try:
+                # It also downloads the videos
+                res = self.extract_info(url)
+            except UnavailableVideoError:
+                self.report_error('unable to download video')
+            except MaxDownloadsReached:
+                self.to_screen('[info] Maximum number of downloaded files reached.')
+                raise
+            else:
+                if self.params.get('dump_single_json', False):
+                    self.to_stdout(json.dumps(res))
         return_obj['json'] = res
         return_obj['retcode'] = self._download_retcode
         return return_obj
-
     def download_with_info_file(self, info_filename):
         with contextlib.closing(fileinput.FileInput(
                 [info_filename], mode='r',
